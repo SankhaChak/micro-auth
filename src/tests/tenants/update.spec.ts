eb942168c40c6085a9cf7a7bb2340393a5cb1dd9
SankhaChak <anonproductionspro@gmail.com>
2024-07-20 17:47:39 +0530

    fix: 🐞 add auth guard to update tenant routes

--- src/tests/tenants/update.spec.ts
+++ src/tests/tenants/update.spec.ts
@@ -1,16 +1,32 @@
+import createJWKSMock from "mock-jwks";
 import request from "supertest";
 import { DataSource } from "typeorm";
 import app from "../../app";
 import { AppDataSource } from "../../data-source";
 import { Tenant } from "../../entity/Tenant";
+import { UserRole } from "../../types/auth";
 import { TenantData } from "../../types/tenant";
 
 describe("PATCH /tenants/:id", () => {
   let dataSource: DataSource;
+  let jwks: ReturnType<typeof createJWKSMock>;
+  let adminToken: string;
+  let nonAdminAccessToken: string;
 
   const singleTenantEndpoint = "/tenants/1";
 
   beforeAll(async () => {
+    jwks = createJWKSMock("http://localhost:5001");
+
+    jwks.start();
+    adminToken = jwks.token({
+      sub: "1",
+      role: UserRole.Admin
+    });
+    nonAdminAccessToken = jwks.token({
+      sub: "2",
+      role: UserRole.Customer
+    });
     dataSource = await AppDataSource.initialize();
   });
 
@@ -35,23 +51,30 @@ describe("PATCH /tenants/:id", () => {
   });
 
   afterAll(async () => {
+    jwks.stop();
     await dataSource.destroy();
   });
 
   describe("Update tenant with a particular id", () => {
     it("should return 200 status code", async () => {
-      const response = await request(app).patch(singleTenantEndpoint).send({
-        name: "Updated Tenant",
-        address: "Updated Address"
-      });
+      const response = await request(app)
+        .patch(singleTenantEndpoint)
+        .set("Cookie", [`accessToken=${adminToken};`])
+        .send({
+          name: "Updated Tenant",
+          address: "Updated Address"
+        });
       expect(response.statusCode).toBe(200);
     });
 
     it("should update the tenant with the provided id", async () => {
-      await request(app).patch(singleTenantEndpoint).send({
-        name: "Updated Tenant",
-        address: "Updated Address"
-      });
+      await request(app)
+        .patch(singleTenantEndpoint)
+        .set("Cookie", [`accessToken=${adminToken};`])
+        .send({
+          name: "Updated Tenant",
+          address: "Updated Address"
+        });
 
       const updatedTenant = await dataSource.getRepository(Tenant).findOne({
         where: { id: 1 }
@@ -62,18 +85,24 @@ describe("PATCH /tenants/:id", () => {
     });
 
     it("should return 404 status code if tenant with the provided id does not exist", async () => {
-      const response = await request(app).patch("/tenants/100").send({
-        name: "Updated Tenant",
-        address: "Updated Address"
-      });
+      const response = await request(app)
+        .patch("/tenants/100")
+        .set("Cookie", [`accessToken=${adminToken};`])
+        .send({
+          name: "Updated Tenant",
+          address: "Updated Address"
+        });
 
       expect(response.statusCode).toBe(404);
     });
 
     it("should update only the provided fields", async () => {
-      await request(app).patch(singleTenantEndpoint).send({
-        name: "Updated Tenant"
-      });
+      await request(app)
+        .patch(singleTenantEndpoint)
+        .set("Cookie", [`accessToken=${adminToken};`])
+        .send({
+          name: "Updated Tenant"
+        });
 
       const updatedTenant = await dataSource.getRepository(Tenant).findOne({
         where: { id: 1 }
@@ -82,9 +111,12 @@ describe("PATCH /tenants/:id", () => {
       expect(updatedTenant?.name).toBe("Updated Tenant");
       expect(updatedTenant?.address).toBe(`Address 0`);
 
-      await request(app).patch(singleTenantEndpoint).send({
-        address: "Updated Address"
-      });
+      await request(app)
+        .patch(singleTenantEndpoint)
+        .set("Cookie", [`accessToken=${adminToken};`])
+        .send({
+          address: "Updated Address"
+        });
 
       const updatedTenant2 = await dataSource.getRepository(Tenant).findOne({
         where: { id: 1 }
@@ -95,10 +127,13 @@ describe("PATCH /tenants/:id", () => {
     });
 
     it("should only update the tenant with the provided id", async () => {
-      await request(app).patch(singleTenantEndpoint).send({
-        name: "Updated Tenant",
-        address: "Updated Address"
-      });
+      await request(app)
+        .patch(singleTenantEndpoint)
+        .set("Cookie", [`accessToken=${adminToken};`])
+        .send({
+          name: "Updated Tenant",
+          address: "Updated Address"
+        });
 
       const tenants = await dataSource.getRepository(Tenant).find();
 
@@ -114,17 +149,43 @@ describe("PATCH /tenants/:id", () => {
     });
 
     it("should return 400 status code if invalid fields are provided", async () => {
-      const response = await request(app).patch(singleTenantEndpoint).send({
-        invalidField: "Invalid Field"
-      });
+      const response = await request(app)
+        .patch(singleTenantEndpoint)
+        .set("Cookie", [`accessToken=${adminToken};`])
+        .send({
+          invalidField: "Invalid Field"
+        });
 
       expect(response.statusCode).toBe(400);
     });
 
     it("should return 400 status code if no fields are provided", async () => {
-      const response = await request(app).patch(singleTenantEndpoint).send({});
+      const response = await request(app)
+        .patch(singleTenantEndpoint)
+        .set("Cookie", [`accessToken=${adminToken};`])
+        .send({});
 
       expect(response.statusCode).toBe(400);
+    });
+
+    it("should return 401 status code if user is not authenticated", async () => {
+      const response = await request(app).patch(singleTenantEndpoint).send({
+        name: "Updated Tenant",
+        address: "Updated Address"
+      });
+      expect(response.statusCode).toBe(401);
+    });
+
+    it("should return 403 status code if user is not admin", async () => {
+      const response = await request(app)
+        .patch(singleTenantEndpoint)
+        .set("Cookie", [`accessToken=${nonAdminAccessToken};`])
+        .send({
+          name: "Updated Tenant",
+          address: "Updated Address"
+        });
+
+      expect(response.statusCode).toBe(403);
     });
   });
 });
